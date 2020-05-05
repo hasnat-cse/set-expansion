38b718144d1a38b9ed4f8c5968d5c4e904e4bb7f
Arif Hasnat <hasnat@ualberta.ca>
2020-05-04 19:09:39 -0600

    new version of modified seisa

--- seisa.py
+++ seisa.py
@@ -308,3 +308,94 @@ def dynamic_seisa_optimized(seed_entities, entity2features, feature2entities, al
 
     print("Expanded entities after Dynamic Seisa: %s" % expanded_entities)
     return expanded_entities
+
+
+def get_sim_score(expanded_entities, entity2features, feature2entities):
+    entity2related_entities = {}
+
+    for entity in expanded_entities:
+        entity2related_entities[entity] = set()
+        for feature in entity2features[entity]:
+            entity2related_entities[entity] = entity2related_entities[entity].union(feature2entities[feature])
+
+    entity2total_sim = {}
+    for entity in expanded_entities:
+        for related_entity in entity2related_entities[entity]:
+
+            if related_entity in entity2total_sim:
+                # todo: better equation
+                entity2total_sim[related_entity] += get_jaccard_sim(related_entity, entity, entity2features)
+
+            else:
+                entity2total_sim[related_entity] = get_jaccard_sim(related_entity, entity, entity2features)
+
+    entity2sim_score = {}
+    for entity in entity2total_sim:
+        entity2sim_score[entity] = entity2total_sim[entity] / len(expanded_entities)
+
+    return entity2sim_score
+
+
+# expand the set of seedEntities and return entities by order
+def the_best_till_now(seed_entities, entity2features, feature2entities, alpha, k):
+    global entity2entity_sim
+    entity2entity_sim = {}
+
+    print("Seed entities inside algorithm: %s" % seed_entities)
+
+    rel_score_dict = get_sim_score(seed_entities, entity2features, feature2entities)
+
+    print("number of candidate entities: %s" % len(rel_score_dict))
+
+    sorted_entities, sorted_scores = get_sorted_key_and_value_based_on_value(rel_score_dict)
+
+    threshold = util.get_otsu_threshold(sorted_scores)
+
+    print("Threshold: %s" % threshold)
+
+    if threshold <= k:
+        k = threshold
+
+    expanded_entities = sorted_entities[:k]
+
+    print("Expanded entities after step 0: %s" % expanded_entities)
+
+    iter = 1
+    while True:
+        sim_score_dict = get_sim_score(expanded_entities, entity2features, feature2entities)
+        print("number of candidate entities: %s" % len(sim_score_dict))
+
+        combined_score_dict = {}
+
+        for entity in sim_score_dict:
+            if entity in rel_score_dict:
+                combined_score_dict[entity] = alpha * rel_score_dict[entity] + (1 - alpha) * sim_score_dict[entity]
+
+            else:
+                combined_score_dict[entity] = (1 - alpha) * sim_score_dict[entity]
+
+        sorted_candidate_entities, sorted_candidate_scores = get_sorted_key_and_value_based_on_value(combined_score_dict)
+
+        candidate_entities = sorted_candidate_entities[:k]
+
+        sorted_expanded_entities = []
+        for entity in sorted_candidate_entities:
+            if entity in expanded_entities:
+                sorted_expanded_entities.append(entity)
+
+        expanded_entities = sorted_expanded_entities
+
+        if set(candidate_entities) != set(expanded_entities):
+            new_candidate_entities = [entity for entity in candidate_entities if entity not in expanded_entities]
+            expanded_entities.pop()
+            expanded_entities.append(new_candidate_entities[0])
+            print("Expanded entities after step %s: %s" % (iter, expanded_entities))
+
+        else:
+            break
+
+        iter += 1
+
+    print("Expanded entities length: %s" % len(expanded_entities))
+    print("Expanded entities after Algorithm: %s" % expanded_entities)
+    return expanded_entities
